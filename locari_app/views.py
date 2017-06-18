94127f47853949a968eac9e202bab1030f7f57c9
uehara <b1015062@fun.ac.jp>
2017-06-19 02:14:02 +0900

    add mypage

--- locari_app/views.py
+++ locari_app/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'locari_app/index.html')
+
+
+def mypage(request):
+    return render(request, 'locari_app/mypage.html')
